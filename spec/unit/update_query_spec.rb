feb718dd6ab1d6576eb112fa8ecf1e599f9de5c0
Willem van Bergen <willem@vanbergen.org>
2010-12-16 04:03:10 -0500

    Fixed some descriptions of update query spec.

--- spec/unit/update_query_spec.rb
+++ spec/unit/update_query_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe SQLTree::Node::UpdateQuery do
 
-  it "should parse a delete query without WHERE clause correctly" do
+  it "should parse an UPDATE query without WHERE clause correctly" do
     update = SQLTree::Node::UpdateQuery["UPDATE table SET field1 = 1, field2 = 5 - 3"]
     update.table.should == SQLTree::Node::TableReference.new("table")
     update.updates.should have(2).items
@@ -13,7 +13,7 @@ describe SQLTree::Node::UpdateQuery do
     update.where.should be_nil
   end
 
-  it "should parse a delete query without WHERE clause correctly" do
+  it "should parse an UPDATE query with WHERE clause correctly" do
     update = SQLTree::Node::UpdateQuery["UPDATE table SET field = 1 WHERE id = 17"]
     update.table.should == SQLTree::Node::TableReference.new("table")
     update.updates.should have(1).item
